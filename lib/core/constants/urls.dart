9a1c77faa1047c9868e9bf88e10dd4e12b57a1db
Punnya PS <YOUR_GITHUB_EMAIL>
2026-09-11 10:44:37 +0530

    add all files

---
+++ lib/core/constants/urls.dart
@@ -0,0 +1,11 @@
+class Urls {
+  static const imageUrl = 'https://image.tmdb.org/t/p/original';
+  static const apiKey = '3e9deeb1735d29e1a370bd47251e0df5';
+  static const baseUrl = 'https://api.themoviedb.org/3';
+
+  static const nowPlaying = '/movie/now_playing';
+  static const topRated = '/movie/top_rated';
+  static const upcomings = '/movie/upcoming';
+  static const tvShows = '/tv/airing_today';
+  static const search = '/search/multi';
+}
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Colors.black;
+  static const netflixRed = Color(0xFFE50914);
+  static const grey = Color(0xFF808080);
+  static const chipBackground = Color(0x33FFFFFF);
+}
